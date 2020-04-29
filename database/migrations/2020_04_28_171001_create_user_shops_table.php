2a614aecd1cc467d8c04f59bf3cca20c182891d6
sahartak <sahartak@yandex.com>
2020-04-29 19:30:00 +0400

    user shop

---
+++ database/migrations/2020_04_28_171001_create_user_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_shops', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('shop_name');
+            $table->smallInteger('plan');
+            $table->boolean('is_active');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_shops');
+    }
+}
